e7e8f9443ec2b3ff2c50510d9643ea600378e108
Mr MRM Development <mubarokrizqy101@gmail.com>
2025-08-01 07:21:50 +0700

    first commit

--- js/load.js
+++ js/load.js
@@ -1,5 +1,6 @@
 const table = document.getElementById("download-table")
 
+<<<<<<< HEAD
 
 
 data.forEach(([version, dev, created, href])=>{
@@ -26,3 +27,17 @@ data.forEach(([version, dev, created, href])=>{
 
 )
 
+=======
+data.forEach(([version, dev, created, href])=>{
+        table.innerHTML += `<tr>
+        
+        <td>${version}</td>
+        <td>${dev}</td>
+        <td>${created}</td>
+        <td><button><a href="${href}" download>Download</a></button></td>
+        
+        </tr>`;
+
+    }
+)
+>>>>>>> 1e0a7e5 (first commit)
