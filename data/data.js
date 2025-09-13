b74242fc3ea708cd16d3bdd80eb591dc0827dfc0
Mr MRM Development <mubarokrizqy101@gmail.com>
2025-09-13 21:55:42 +0700

    first commit

---
+++ data/data.js
@@ -0,0 +1,14 @@
+let data = [
+
+    ["OMERIZ beta 1.0.9", "M. Rizqy Mubarok", "25 July 2025", "omeriz_version/OMERIZ beta a-1.0.9 No Node Module.rar"],
+    ["OMERIZ beta 1.0.8", "M. Rizqy Mubarok, M. Raka Falleh", "25 July 2025", "omeriz_version/OMERIZ beta a-1.0.8 No Node Module.rar"],
+    ["OMERIZ beta 1.0.7", "M. Rizqy Mubarok", "25 July 2025", "omeriz_version/OMERIZ beta a-1.0.7 No Node Module.rar"],
+    ["OMERIZ beta 1.0.6", "M. Rizqy Mubarok", "23 July 2025", "omeriz_version/OMERIZ beta a-1.0.6 No Node Module.rar"],
+    ["OMERIZ beta 1.0.5", "M. Rizqy Mubarok, M. Raka Falleh", "22 July 2025", "omeriz_version/OMERIZ beta a-1.0.5 No Node Module.rar"],
+    ["OMERIZ beta 1.0.4", "M. Rizqy Mubarok", "21 July 2025", "omeriz_version/OMERIZ beta a-1.0.4 No Node Module.rar"],
+    ["OMERIZ beta 1.0.3", "M. Rizqy Mubarok", "20 July 2025", "omeriz_version/OMERIZ beta a-1.0.3 No Node Module.rar"],
+    ["OMERIZ beta 1.0.2", "M. Rizqy Mubarok, Naufal Y. A.", "20 July 2025", "omeriz_version/OMERIZ beta a-1.0.2 No Node Module.rar"],
+    ["OMERIZ beta 1.0.1", "M. Rizqy Mubarok", "10 July 2025", "omeriz_version/OMERIZ beta a-1.0.1 No Node Module.rar"],
+    ["OMERIZ beta 1.0.0", "M. Rizqy Mubarok", "9 July 2025", "omeriz_version/OMERIZ beta a-1.0.0 No Node Module.rar"]
+
+];
